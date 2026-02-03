7df069d7b27000ff20ad052a762a3fa5a4629dbb
Zack289 <ozack911@gmail.com>
2026-02-03 16:27:59 +0545

    Added the gsap library for the switching effect of the video

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,8 @@
+import gsap from "gsap";
 import React, { useRef, useState } from "react";
+import Button from "./Button";
+import { TiLocationArrow } from "react-icons/ti";
+import { useGSAP } from "@gsap/react";
 
 function Hero() {
   const [currentIndex, setCurrentIndex] = useState(1);
@@ -16,13 +20,36 @@ function Hero() {
     setCurrentIndex((prevIndex) => (prevIndex % totalVideos) + 1);
   };
 
-  const handleVideoLoad = ()=>{
-    setLoadedVideos(prev => prev + 1)
+  useGSAP(()=>{
+    if(hasClicked){
+      gsap.set('#next-video', {visibility: 'visible'});
+
+      gsap.to('#next-video',{
+        transformOrigin: 'center',
+        scale: 1,
+        width: "100%",
+        height: "100%",
+        duration: 1,
+        ease: 'power1.inOut',
+        onStart:() => nextVideoRef.current.play(),
+      })
+
+      gsap.from('#current-video', {
+        transformOrigin: 'center center',
+        scale: 0,
+        duration: 1.5,
+        ease: "power1.inOut",
+        onStart: ()=>  nextVideoRef.current.play()
+      })
+    }
+  }, {dependencies: [currentIndex],
+      revertOnUpdate: true})
+
+  const handleVideoLoad = () => {
+    setLoadedVideos((prev) => prev + 1);
   };
 
-  
-
-  const getVideoSrc = (index) => `videos/hero-${index}.mp4`
+  const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
   return (
     <div className="relative h-dvh w-screen overflow-x-hidden">
       <div
@@ -31,20 +58,65 @@ function Hero() {
       >
         <div>
           <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
-            <div onClick={handleMiniVdClick} className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100">
-                <video 
-                  ref={nextVideoRef}
-                  src={getVideoSrc(currentIndex + 1)}
-                  loop
-                  muted
-                  id="current-video"
-                  className="size-64 origin-center scale-150 object-cover object-center"
-                  onLoadedData={handleVideoLoad}
-                />
+            <div
+              onClick={handleMiniVdClick}
+              className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
+            >
+              <video
+                ref={nextVideoRef}
+                src={getVideoSrc((currentIndex % totalVideos) + 1)}
+                loop
+                muted
+                id="current-video"
+                className="size-64 origin-center scale-150 object-cover object-center"
+                onLoadedData={handleVideoLoad}
+              />
             </div>
+          </div>
+
+          <video
+            ref={nextVideoRef}
+            src={getVideoSrc(currentIndex)}
+            loop
+            muted
+            id="next-video"
+            className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
+          />
+
+          <video
+            src={getVideoSrc(
+              currentIndex === totalVideos - 1 ? 1 : currentIndex,
+            )}
+            // autoPlay
+            loop
+            muted
+            className="absolute left-0 top-0 size-full object-cover object-center"
+            onLoadedData={handleVideoLoad}
+          />
+        </div>
+
+        <h1 className="secial-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
+          G<b>a</b>ming
+        </h1>
+
+        <div className="absolute left-0 top-0 z-40 size-full">
+          <div className="mt-24 px-5 sm:px-10">
+            <h1 className="special-font hero-heading text-blue-100">
+              redefi <b>n</b>e
+            </h1>
+
+            <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
+              Enter the Metagame Layer <br /> Unlesh the play Economy
+            </p>
+
+            <Button id="watch-trailer" title="Watch Trailer" leftIcon={<TiLocationArrow />} containerClass="!bg-yellow-300 flex-center gap-1"/>
           </div>
         </div>
       </div>
+
+       <h1 className="secial-font hero-heading absolute bottom-5 right-5 text-black">
+          G<b>a</b>ming
+        </h1>
     </div>
   );
 }
